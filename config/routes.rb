be979ab4fc0c1eee6dac42093f668dec6f16d2c8
Rowenda <badbat_69@hotmail.com>
2023-11-29 10:12:13 +0100

    maj ruby/rails and refacto

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :restaurants, except: [:destroy, :edit, :update] do
+    resources :reviews, only: [:create]
+  end
+end
